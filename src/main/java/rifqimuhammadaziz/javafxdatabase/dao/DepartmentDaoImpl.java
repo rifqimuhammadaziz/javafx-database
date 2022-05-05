91ac8eba04124aa37926ee6d26223ede2c3ef8b9
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-05-05 11:12:18 +0700

    Dao implementation on entity

---
+++ src/main/java/rifqimuhammadaziz/javafxdatabase/dao/DepartmentDaoImpl.java
@@ -0,0 +1,59 @@
+package rifqimuhammadaziz.javafxdatabase.dao;
+
+import rifqimuhammadaziz.javafxdatabase.entity.Department;
+import rifqimuhammadaziz.javafxdatabase.entity.Faculty;
+import rifqimuhammadaziz.javafxdatabase.util.DaoService;
+import rifqimuhammadaziz.javafxdatabase.util.MySQLConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentDaoImpl implements DaoService<Department> {
+
+    @Override
+    public List<Department> getAll() throws SQLException, ClassNotFoundException {
+        List<Department> departments = new ArrayList<>();
+        try (Connection connection = MySQLConnection.createConnection()) {
+            String QUERY = "SELECT d.id, d.name, d.faculty_id, f.name AS faculty_name FROM department d JOIN faculty f ON d.faculty_id = f.id ";
+            try (PreparedStatement ps = connection.prepareStatement(QUERY)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        Faculty faculty = new Faculty();
+                        faculty.setId(rs.getInt("faculty_id"));
+                        faculty.setName(rs.getString("faculty_name"));
+
+                        Department department = new Department();
+                        department.setId(rs.getInt("id"));
+                        department.setName(rs.getString("name"));
+                        department.setFaculty(faculty);
+                        departments.add(department);
+                    }
+                }
+            }
+        }
+        return departments;
+    }
+
+    @Override
+    public int addData(Department object) throws SQLException, ClassNotFoundException {
+        int result = 0;
+        try (Connection connection = MySQLConnection.createConnection()) {
+            String QUERY = "INSERT INTO department(name, faculty_id) VALUES(?, ?)";
+            try (PreparedStatement ps = connection.prepareStatement(QUERY)) {
+                ps.setString(1, object.getName());
+                ps.setInt(2, object.getFaculty().getId());
+                if (ps.executeUpdate() != 0) {
+                    connection.commit();
+                    result = 1;
+                } else {
+                    connection.rollback();
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/rifqimuhammadaziz/javafxdatabase/dao/FacultyDaoImpl.java
@@ -0,0 +1,52 @@
+package rifqimuhammadaziz.javafxdatabase.dao;
+
+import rifqimuhammadaziz.javafxdatabase.entity.Faculty;
+import rifqimuhammadaziz.javafxdatabase.util.DaoService;
+import rifqimuhammadaziz.javafxdatabase.util.MySQLConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FacultyDaoImpl implements DaoService<Faculty> {
+
+    @Override
+    public List<Faculty> getAll() throws SQLException, ClassNotFoundException {
+        List<Faculty> faculties = new ArrayList<>();
+        try (Connection connection = MySQLConnection.createConnection()) {
+            String QUERY = "SELECT id, name FROM faculty";
+            try (PreparedStatement ps = connection.prepareStatement(QUERY)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        Faculty faculty = new Faculty();
+                        faculty.setId(rs.getInt("id"));
+                        faculty.setName(rs.getString("name"));
+                        faculties.add(faculty);
+                    }
+                }
+            }
+        }
+        return faculties;
+    }
+
+    @Override
+    public int addData(Faculty object) throws SQLException, ClassNotFoundException {
+        int result = 0;
+        try (Connection connection = MySQLConnection.createConnection()) {
+            String QUERY = "INSERT INTO faculty(name) VALUES(?)";
+            try (PreparedStatement ps = connection.prepareStatement(QUERY)) {
+                ps.setString(1, object.getName());
+                if (ps.executeUpdate() != 0) {
+                    connection.commit();
+                    result = 1;
+                } else {
+                    connection.rollback();
+                }
+            }
+        }
+        return result;
+    }
+}
